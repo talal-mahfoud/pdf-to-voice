489862360641b3f0b7a21a1ff46a74c2310787e8
talal-mahfoud <abushhiwat@gmail.com>
2025-12-30 01:41:14 +0300

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -93,8 +93,10 @@ def index():
 
     return render_template('index.html')
 
+
 if __name__ =="__main__":
     with app.app_context():
          db.create_all()
          port = int(os.environ.get("PORT", 18012))
     app.run(host='0.0.0.0',port=port)
+
